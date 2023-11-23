23687332accd4ea3d7ee3bf151ae300fe0b435fb
Alan Valdevenito <avaldevenito@fi.uba.ar>
2023-11-23 12:01:31 -0300

    Creando la clase Dificultad.
    La clase recibe tres parametros: palabras, cantidad de intentos y
    cantidad de pistas
    
    El tipo de dificultad dependera de estos tres parametros. Cuando se
    quiera elegir una dificultad se tendra que instanciar esta clase con
    los parametros correspondientes. Por ejemplo, si es facil entonces
    sus parametros seran:
    - Lista de palabras faciles de la base de datos
    - Cantidad de intentos igual a 7
    - Cantidad de pistas igual a 3
    
    Por el momento decidi implementar solo una clase Dificultad que
    generalice las tres dificultades ya que solo se diferencian en
    estos 3 atributos.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-def hola():
-    print("¡Hola mundo!")
+from dificultad import Dificultad
 
-hola()
+def main():
+
+    dificultad = Dificultad(["a", "b", "c"], 7, 3)
+
+    print(f"Palabras: {dificultad.get_palabras()}")
+    print(f"Intentos: {dificultad.get_intentos()}")
+    print(f"Pistas: {dificultad.get_pistas()}")
+
+    print(f"Quedan intentos: {dificultad.quedan_intentos()}")
+    print(f"Quedan pistas: {dificultad.quedan_pistas()}")
+
+    return 0
+
+main()
