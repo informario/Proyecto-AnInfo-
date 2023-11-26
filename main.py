adf165d50609a6038fd60138264e1a8700ef4034
iinaki <llorensinaki22@gmail.com>
2023-11-26 18:08:23 -0300

    refactor main menu

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from main_menu import *
-from estado_juego import EstadoJuego
-from dificultad import Dificultad
-from juego_ahorcado import JuegoAhorcado
 
 def main():
-    juego = JuegoAhorcado(Dificultad.FACIL)
 
-    if input("-") == "1":
-        juego.iniciar()
-        if juego.estado == EstadoJuego.ABANDONADO:
-            main()
+    main_menu()
 
 main()
