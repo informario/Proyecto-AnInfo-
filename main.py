3dd0bb2a382f3d3fbb9c8be3b25b634b79058961
iinaki <llorensinaki22@gmail.com>
2023-12-07 11:19:22 -0300

    menu and session handler+

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from session_handler import *
 
 def main():
     session_handler = SessionHandler()
-    user_name, score, clues = session_handler.run()
+    user_name, score, basic_clues, hint_clues = session_handler.run()
 
     if user_name is not None:
-        game = GameController(score, clues)
-        final_score, final_basic_clues = game.run()
+        game = GameController(score, basic_clues, hint_clues)
+        final_score, final_basic_clues, final_hint_clues = game.run()
 
-        session_handler.update(user_name, final_score, final_basic_clues) # Hay que implementar la logica para devolver la cantidad de pistas acumuladas
+        session_handler.update(user_name, final_score, final_basic_clues, final_hint_clues) # Hay que implementar la logica para devolver la cantidad de pistas acumuladas
 
 main()
